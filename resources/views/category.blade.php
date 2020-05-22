3b5ece5b95d96b650815bc45be77ba8b600479cf
Nikson09 <niksgreek@gmail.com>
2020-05-22 17:57:59 +0300

    -add product

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -30,14 +30,66 @@
 
 
 
-         <div class="col-9 padding-right">
-<div class="Items_Back">
+<div class="col-9 padding-right" >
+    <div class="features_items">
+         <div class="Items_Back ">
+
+
+                     <div class="row justify-content-center ">
+
+
+                 @foreach ($products as $product)
+                     <div class="col-4">
+                         <div class="product-image-wrapper">
+                             <div class="single-products">
+                                 <div class="productinfo text-center">
+
+                                     <div class="row justify-content-center ">
+                                     <a href="/product/{{$product ->id}}">  <img  class="lazy" src="{{asset('/img/product/$product ->id')}}.jpg"  width="auto" height="215,783" alt="" /></a>
+                                     </div>
+
+
+                                     <div class="row justify-content-center " >
+
+
+
+                                                     </div>
+                                     <p>
+                                         <a class="main-goods__title" href="/product/{{$product ->id}}">
+                                             {{$product ->name}}
+                                         </a>
+                                     </p>
 
 
 
 
-</div>
-</div>
+                                                                     </span>
+         <div class="But_add" >
+         <div class="row justify-content-center " >
+         <div class="row justify-content-center " >
+
+                                     <h5 class="pric">{{$product ->price}}грн</h5>
+
+                                     </div>
+                                     <a href="#" class="btn btn-default add-to-cart" data-id="{{$product ->id}}"><i class="fa fa-shopping-cart"></i>В корзину</a>
+                                     </div>
+                                 </div>
+                             </div>  </div>
+                         </div>
+                     </div>
+                @endforeach
+
+
+
+         </div>
+
+         </div>
+
+         </div>
+
+             </div>
+         </div>
+         </div>
 </div>
             </div>
 </section>
